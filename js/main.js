c4a6e79ff6bd3b252378108486f402ae937c9d73
Jon Rish <jon.m.rish@gmail.com>
2013-08-17 16:20:35 -0400

    add jquery popup plugin / faq & footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+$(document).ready( function() {
+
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+	$('.FAQ').popup({
+		content: '/pop_ups/FAQ.html',
+		height: 350
+	});
+
+});
 
 
