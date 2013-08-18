bc98b53654f8e5376aa4bf11143fa6875d68626e
Jon Rish <jon.m.rish@gmail.com>
2013-08-17 23:33:45 -0400

    add params to js popup / faq, contact & about me

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ $(document).ready( function() {
 
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
-	FooterPopUp('.FAQ', '/pop_ups/FAQ.html', 300)
+	FooterPopUp('.FAQ', '/pop_ups/FAQ.html', 300, 700);
+	FooterPopUp('.about-me', '/pop_ups/about_me.html', 400, 720);
+	FooterPopUp('.contact', '/pop_ups/contact_me.html', null, 500);
 
-	
 });
 
-function FooterPopUp(element, content, height) {
+function FooterPopUp(element, content, height, width) {
 	$(element).popup({
 		content: content,
-		height: height
+		height: height,
+		width: width
 	});
 }
