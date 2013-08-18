9a37f6e2ff55dfb692c3f575fa4799f7ce5ddf71
Jon Rish <jon.m.rish@gmail.com>
2013-08-17 22:24:24 -0400

    footer links / refactor js popup function

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@ $(document).ready( function() {
 
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
-	$('.FAQ').popup({
-		content: '/pop_ups/FAQ.html',
-		height: 350
-	});
+	FooterPopUp('.FAQ', '/pop_ups/FAQ.html', 300)
 
+	
 });
 
-
+function FooterPopUp(element, content, height) {
+	$(element).popup({
+		content: content,
+		height: height
+	});
+}
